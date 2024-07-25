d72dd1944aa5dc4858d48a94935b8e2c99a0bb39
SpiritedAwayCN <541845219@qq.com>
2024-07-25 21:48:20 +0800

    frontend with output shapes/strides

--- python/mirage/wrapper.py
+++ python/mirage/wrapper.py
@@ -79,6 +79,9 @@ PyMODINIT_FUNC PyInit___mirage_launcher(void) {
 }
 """
 
+def gen_empty_tensor(alloc_size, shape, stride, device, dtype=torch.float16):
+    return torch.empty(alloc_size, dtype=dtype, device=device).as_strided(shape, stride)
+
 class PyGraphWrapper:  
     def __init__(self, graph):  
         self.pygraph = graph
@@ -95,28 +98,19 @@ class PyGraphWrapper:
         
         assert self.run is not None, "The graph is not compiled yet."
         
-        # TODO: set buffer_size to a given value
-        
         input_tensors = kwargs.get("inputs", [])
-        output_nodes = kwargs.get("outputs", [])
         
         # TODO: dtype and device
-        buffer_tensor = torch.empty(results[0], dtype=torch.uint8, device=input_tensors[0].device).contiguous()
-        output_shapes = [[node.dim(i) for i in range(node.num_dims)] for node in output_nodes]
-        # print("storage ptr of output[0]:", hex(output_tensors[0].storage().data_ptr()))
-        print("Shapes of output tensors:", output_shapes)
-        # TODO: size of output tensors
-        output_tensors = [torch.empty(4096, dtype=torch.float16, device=input_tensors[0].device).as_strided(
-            output_shapes[i], results[1][i]) for i in range(len(output_shapes))]
+        buffer_tensor = torch.empty(results['buf_size'], 
+                                    dtype=torch.uint8, device=input_tensors[0].device).contiguous()
+        
+        output_tensors = [gen_empty_tensor(meta['alloc_size'], meta['shape'], meta['strides'],
+                                           device=input_tensors[0].device, dtype=torch.float16)
+                          for meta in results['output_directives']]
         
         buffer_tensor_ptr = buffer_tensor.data_ptr()
         input_tensors_ptr = [tensor.data_ptr() for tensor in input_tensors]
         output_tensors_ptr = [tensor.data_ptr() for tensor in output_tensors]
-        
-        print("Input tensors ptr:", [hex(ptr) for ptr in input_tensors_ptr])
-        print("Output tensors ptr:", [hex(ptr) for ptr in output_tensors_ptr])
-        print("Buffer tensor ptr:", hex(buffer_tensor_ptr))
-        print("Buffer size:", buffer_tensor.storage().size())
         
         self.run(input_tensors_ptr, output_tensors_ptr, buffer_tensor_ptr)
         
@@ -130,27 +124,27 @@ class PyGraphWrapper:
         input_strides = [tensor.stride() for tensor in input_tensors]
         target_cc = kwargs.get("target_cc", torch.cuda.get_device_properties(0).major * 10 
                                + torch.cuda.get_device_properties(0).minor)
-        print('input_strides:', input_strides)
         
         result = generate_cuda_program(self.pygraph, 
                                        target_cc=target_cc, 
                                        input_strides=input_strides, 
                                        output_tensors=kwargs.get("outputs", []))
-        print(result[1], result[2])
+        # print(result)
         
-        MIRAGE_ROOT = '../../mirage'
+        MIRAGE_ROOT = os.environ.get('MIRAGE_ROOT', '../../mirage')
         
-        if True:
-            tempdir = './test/'
-        # with tempfile.TemporaryDirectory() as tempdir:
+        # if True:
+        #     tempdir = './test/'
+        with tempfile.TemporaryDirectory() as tempdir:
             FILE_NAME = os.path.join(tempdir, "test.cu")
             so_path = os.path.join(tempdir, "test.cpython-38-x86_64-linux-gnu.so")
         
-            # TODO: why result is a tuple?
             with open(FILE_NAME, "w") as f:
-                f.write(result[0] + HARD_CODE)
+                f.write(result['code'] + HARD_CODE)
             
             cc = shutil.which("nvcc")
+            if cc is None:
+                raise RuntimeError("nvcc not found. Please make sure you have installed CUDA.")
             
             # This function was renamed and made public in Python 3.10
             if hasattr(sysconfig, 'get_default_scheme'):
@@ -175,7 +169,6 @@ class PyGraphWrapper:
             ret = subprocess.check_call(cc_cmd)
             # so_path = './test.cpython-38-x86_64-linux-gnu.so'
             
-            print("Loading the shared object file from path: {}".format(so_path))
             import importlib.util
             spec = importlib.util.spec_from_file_location("__mirage_launcher", so_path)
             mod = importlib.util.module_from_spec(spec)
@@ -183,5 +176,5 @@ class PyGraphWrapper:
             self.run = getattr(mod, "launch")
         
         self._is_compiled = True
-        self._cached_results = result[1:]
+        self._cached_results = result
         return self._cached_results
