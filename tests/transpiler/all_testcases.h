981aa9e1bf4bdd9227040f73e7ced387d1e50e4e
interestingLSY <interestingLSY@gmail.com>
2024-08-06 04:12:07 +0800

    Add unit test for the transpiler

---
+++ tests/transpiler/all_testcases.h
@@ -0,0 +1,10 @@
+// Add new testcases here
+
+#pragma once
+
+#include "testcases/kernel/elemwise.h"
+#include "testcases/kernel/matmul.h"
+#include "testcases/kernel/reduction.h"
+
+#include "testcases/threadblock/elemwise.h"
+#include "testcases/threadblock/matmul.h"
