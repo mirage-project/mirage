58ef68bd2832a4ffb5d10316a0a4dd3c1e391081
Chun'an Shi <44977219+SpiritedAwayCN@users.noreply.github.com>
2024-08-14 00:12:18 +0800

    Update the setup workflow for packaging. (#45)
    * temp version
    
    * Update setup as package
    
    * Improve CMakeList.txt for develop
    
    * Remove output.log
    
    * Update INSTALL.md

--- python/mirage/__init__.py
+++ python/mirage/__init__.py
@@ -1,4 +1,13 @@
-from .core import *
+import os
+
+try:
+    from .core import *
+except ImportError:
+    import z3
+    _z3_lib = os.path.join(os.path.dirname(z3.__file__), 'lib')
+    os.environ['LD_LIBRARY_PATH'] = f"{_z3_lib}:{os.environ.get('LD_LIBRARY_PATH','LD_LIBRARY_PATH')}"
+    
+    from .core import *
 
 class InputNotFoundError(Exception):
     """Raised when cannot find input tensors """
