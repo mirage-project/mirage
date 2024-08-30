9e0b028d447402d099984235aa2696b7012718fb
interestingLSY <interestingLSY@gmail.com>
2024-08-30 12:24:08 +0800

    Refine TB reduction kernel to avoid using stride

--- include/mirage/transpiler/runtime/threadblock/reduction.h
+++ include/mirage/transpiler/runtime/threadblock/reduction.h
@@ -1,6 +1,7 @@
 // redyctuib.h - Implementation of thread block level reduction operators
 #pragma once
 
+#include "cute/stride.hpp"
 #include <cassert>
 
 #include <cute/layout.hpp>
@@ -28,27 +29,28 @@ public:
   static constexpr int REDUCTION_FACTOR =
       get<REDUCTION_DIM>(shape(SrcLayout{})) /
       get<REDUCTION_DIM>(shape(DstLayout{}));
-  static constexpr int REDUCTION_DIM_STRIDE =
-      get<REDUCTION_DIM>(stride(SrcLayout{}));
 
-  // Refer to runtime/kernel/reduction.h for the wisdom of the following code
-  using DstInSrcLayout = decltype(make_layout(
-      shape(DstLayout{}),
-      replace<REDUCTION_DIM>(stride(SrcLayout{}),
-                             Int<REDUCTION_DIM_STRIDE * REDUCTION_FACTOR>{})));
-  static_assert(is_static_v<DstInSrcLayout>);
+  using SrcShapeStride = decltype(stride(make_layout(shape(SrcLayout{}), LayoutLeft{})));
+  using SrcReductionDimCoordStride = decltype(get<REDUCTION_DIM>(SrcShapeStride{}));
+  static constexpr int SRC_REDUCTION_DIM_COORD_STRIDE = SrcReductionDimCoordStride::value;
+  using DstCoord2SrcCoord = decltype(make_layout(
+    shape(DstLayout{}),
+    replace<REDUCTION_DIM>(SrcShapeStride{}, Int<REDUCTION_FACTOR * SRC_REDUCTION_DIM_COORD_STRIDE>{})
+  ));
+  static_assert(is_static_v<DstCoord2SrcCoord>);
 
   static __device__ __forceinline__ void
       run(T *__restrict__ dst, T const *__restrict__ src, int thread_idx) {
-    auto dst_in_src_layout = DstInSrcLayout{};
+    auto src_layout = SrcLayout{};
     auto dst_layout = DstLayout{};
+    auto dst_coord2src_coord = DstCoord2SrcCoord{};
     for (int dst_elem_idx = thread_idx; dst_elem_idx < DST_NUMEL;
          dst_elem_idx += NUM_THREADS) {
-      int src_elem_idx = dst_in_src_layout(dst_elem_idx);
+      int src_elem_idx = dst_coord2src_coord(dst_elem_idx); // The logical index of the first element in the reduction group
       T result = (T)0;
       CUTE_UNROLL
       for (int i = 0; i < REDUCTION_FACTOR; ++i) {
-        result += src[src_elem_idx + i * REDUCTION_DIM_STRIDE];
+        result += src[src_layout(src_elem_idx + i*SRC_REDUCTION_DIM_COORD_STRIDE)];
       }
       auto dst_phy_pos = dst_layout(dst_elem_idx);
       Epilogue::run(result, dst, dst_phy_pos);
