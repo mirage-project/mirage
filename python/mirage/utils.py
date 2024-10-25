64d9263e93eba406cd88897294314a0c370a20bc
Zhihao Jia <zhihao@cmu.edu>
2024-10-25 23:43:23 +0000

    update utils

--- python/mirage/utils.py
+++ python/mirage/utils.py
@@ -1,11 +1,17 @@
-class GPUInfo:
-
-    # Add more archs
-    MAX_SHARED_MEMORY_MAP = {
-        80: 96 * 1024,  # Ampere
-        86: 96 * 1024,  # A6000
-    }
-
-
+# This function returns the shared memory limit (in bytes)
+# for the given GPU hardware architecture
 def get_shared_memory_capacity(target_cc):
-    return GPUInfo.MAX_SHARED_MEMORY_MAP.get(target_cc, 0)
+    if target_cc == 80:
+        # A100 GPUs
+        return 163 * 1024
+    elif target_cc == 86:
+        # A5000 GPUs
+        return 99 * 1024
+    elif target_cc == 89:
+        # A6000 GPUs
+        return 99 * 1024
+    elif target_cc == 90:
+        # H100 GPUs
+        return 223 * 1024
+    else:
+        assert False, "Unsupported compute capacity: {}".format(target_cc)
