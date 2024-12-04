1134988302396bbc779ad2d54d01694085220557
Gabriele Oliaro <goliaro@cs.cmu.edu>
2024-12-04 20:10:51 +0000

    upd

--- demo/benchmark/rmsnorm.py
+++ demo/benchmark/rmsnorm.py
@@ -6,26 +6,28 @@ import argparse
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--bs', type=int, default=1)
-    parser.add_argument('--file', type=str, default='moe.json')
+    parser.add_argument('--file', type=str, default='rmsnorm.json')
     args = parser.parse_args()
     batch_size = args.bs
     filename = args.file
 
     graph = mi.new_kernel_graph()
-    X = graph.new_input(dims=(8, 8, 4096), dtype=mi.float16)
-    A = graph.new_input(dims=(8, 4096, 4096), dtype=mi.float16)
-    B = graph.new_input(dims=(8, 4096, 4096), dtype=mi.float16)
-    D = graph.matmul(X, A)
-    E = graph.exp(D)
-    O = graph.matmul(E, B)
+    X = graph.new_input(dims=(16, 4096), dtype=mi.float16)
+    W = graph.new_input(dims=(4096, 6144), dtype=mi.float16)
+    D = graph.rms_norm(X, normalized_shape=(4096,))
+    O = graph.matmul(D, W)
     graph.mark_output(O)
-    optimized_graph = graph.superoptimize(previous_checkpoint="attention")
+    optimized_graph = graph.superoptimize(config="mlp", previous_checkpoint=filename)
 
     input_tensors = [
-        torch.randn(8, 8, 4096, dtype=torch.float16, device='cuda:0'),
-        torch.randn(8, 4096, 4096, dtype=torch.float16, device='cuda:0'),
-        torch.randn(8, 4096, 4096, dtype=torch.float16, device='cuda:0')
+        torch.randn(16, 4096, dtype=torch.float16, device='cuda:0'),
+        torch.randn(4096, 4096, dtype=torch.float16, device='cuda:0'),
     ]
+
+    outputs = optimized_graph(inputs=input_tensors)
+    output = outputs[0]
+    print(output.shape)
+    print(output.stride(0), output.stride(1))
 
     for _ in range(16):
         optimized_graph(inputs=input_tensors)
@@ -41,4 +43,3 @@ if __name__ == "__main__":
     mean_syn = curr_time / 1000
 
     print("Best muGraph run time (ms): ", mean_syn)
-
