b94996a7e23b3d06a370cfb8e94c4e62a702696c
Jianan Ji <jji2@catalyst-0-9.eth>
2025-03-10 21:55:15 -0400

    fix MIRAGE_ROOT for triton

--- python/mirage/kernel.py
+++ python/mirage/kernel.py
@@ -510,9 +510,9 @@ class KNGraph:
             return all_graphs
         elif backend == "triton":
             MIRAGE_ROOT = os.environ.get(
-                "MIRAGE_ROOT", os.path.join(os.path.dirname(__file__), "../../include")
+                "MIRAGE_ROOT", os.path.join(os.path.dirname(__file__), "../..")
             )
-            os.environ["KERNELS_PATH"] = os.path.join(MIRAGE_ROOT, "mirage/transpiler/runtime") # for triton
+            os.environ["KERNELS_PATH"] = os.path.join(MIRAGE_ROOT, "include/mirage/transpiler/runtime") # for triton
             best_graph, best_file_path, best_output_shapes = profile_and_select_best_graph(all_graphs, 
                                                  target_cc=torch.cuda.get_device_properties(0).major * 10 
                                                  + torch.cuda.get_device_properties(0).minor,
