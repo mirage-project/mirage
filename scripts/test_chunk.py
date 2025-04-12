74989b61f337341b9d9d39916a631d3235bd579e
AMKCode <aomankit2002@gmail.com>
2025-04-12 18:34:28 -0400

    test chunk

---
+++ scripts/test_chunk.py
@@ -0,0 +1,38 @@
+import mirage as mi
+import numpy as np
+import torch
+
+if __name__ == "__main__":
+    graph = mi.new_kernel_graph()
+    X = graph.new_input(dims=(16, 4096), dtype=mi.float16)
+    W = graph.new_input(dims=(4096, 6144), dtype=mi.float16)
+    D1, D2 = graph.chunk(X, 2, 0)
+    D = graph.add(D1, D2)
+    O = graph.matmul(D, W)
+    graph.mark_output(O)
+    optimized_graph = graph.superoptimize(config="mlp")
+
+    input_tensors = [
+        torch.randn(16, 4096, dtype=torch.float16, device='cuda:0'),
+        torch.randn(4096, 4096, dtype=torch.float16, device='cuda:0'),
+    ]
+
+    outputs = optimized_graph(inputs=input_tensors)
+    output = outputs[0]
+    print(output.shape)
+    print(output.stride(0), output.stride(1))
+
+    for _ in range(16):
+        optimized_graph(inputs=input_tensors)
+
+    torch.cuda.synchronize()
+    starter, ender = torch.cuda.Event(enable_timing=True), torch.cuda.Event(enable_timing=True)
+    starter.record()
+    for _ in range(1000):
+        optimized_graph(inputs=input_tensors)
+    ender.record()
+    torch.cuda.synchronize()
+    curr_time = starter.elapsed_time(ender)
+    mean_syn = curr_time / 1000
+
+    print("Best muGraph run time (ms): ", mean_syn)
