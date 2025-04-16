05e60a52aa95ed5f6161c4d59213a0b27ce053db
undefined-c0der <jiangjc22@mails.tsinghua.edu.cn>
2025-04-16 17:17:40 -0400

    update reference mugraph

--- demo/reference_mugraphs/group_query_attention_online.py
+++ demo/reference_mugraphs/group_query_attention_online.py
@@ -1,12 +1,11 @@
 import mirage as mi
-import numpy as np
 import torch
 
 if __name__ == "__main__":
     graph = mi.new_kernel_graph()
-    Q = graph.new_input(dims=(2, 64, 64), dtype=mi.float32)
-    K = graph.new_input(dims=(2, 64, 64), dtype=mi.float32)
-    V = graph.new_input(dims=(2, 64, 64), dtype=mi.float32)
+    Q = graph.new_input(dims=(2, 64, 64), dtype=mi.float16)
+    K = graph.new_input(dims=(2, 64, 64), dtype=mi.float16)
+    V = graph.new_input(dims=(2, 64, 64), dtype=mi.float16)
     tbgraph1 = mi.new_threadblock_graph(
         grid_dim=(2, 1, 4), block_dim=(128, 1, 1), forloop_range=1, reduction_dimx=64
     )
@@ -22,36 +21,35 @@ if __name__ == "__main__":
     tbgraph1.new_output(stensor=bO2, output_map=(0, 2, 1))
     O = graph.customized([Q, K, V], tbgraph1)
 
-    tbgraph2 = mi.new_threadblock_graph(
-        grid_dim=(2, 1, 1), block_dim=(128, 1, 1), forloop_range=1, reduction_dimx=64
-    )
-    bA = tbgraph2.new_input(dtensor=O[0], input_map=(0, 1, -1), forloop_dim=-1)
-    bB = tbgraph2.new_input(dtensor=O[1], input_map=(0, 1, -1), forloop_dim=-1)
-    bA = tbgraph2.forloop_accum(bA, "sum_todimx")
-    bB = tbgraph2.forloop_accum(bB, "sum")
-    bO = tbgraph2.div(bA, bB)
-    tbgraph2.new_output(stensor=bO, output_map=(0, 1, -1))
-    O = graph.customized(O, tbgraph2)
+    # tbgraph2 = mi.new_threadblock_graph(
+    #     grid_dim=(2, 1, 1), block_dim=(128, 1, 1), forloop_range=1, reduction_dimx=64
+    # )
+    # bA = tbgraph2.new_input(dtensor=O[0], input_map=(0, 1, -1), forloop_dim=-1)
+    # bB = tbgraph2.new_input(dtensor=O[1], input_map=(0, 1, -1), forloop_dim=-1)
+    # bA = tbgraph2.forloop_accum(bA, "sum_todimx")
+    # bB = tbgraph2.forloop_accum(bB, "sum")
+    # bO = tbgraph2.div(bA, bB)
+    # tbgraph2.new_output(stensor=bO, output_map=(0, 1, -1))
+    # O = graph.customized(O, tbgraph2)
 
     graph.mark_output(O[0])
+    graph.mark_output(O[1])
+
     input_tensors = [
-        torch.randn(2, 64, 64, dtype=torch.float32, device="cuda:0"),
-        torch.randn(2, 64, 64, dtype=torch.float32, device="cuda:0"),
-        torch.randn(2, 64, 64, dtype=torch.float32, device="cuda:0"),
+        torch.randn(2, 64, 64, dtype=torch.float16, device="cuda:0"),
+        torch.randn(2, 64, 64, dtype=torch.float16, device="cuda:0"),
+        torch.randn(2, 64, 64, dtype=torch.float16, device="cuda:0"),
     ]
 
-    tQ, tK, tV = input_tensors
-    input_tensors = [tQ, tK, tV]
-    tQ = tQ.float()
-    tK = tK.float()
-    tV = tV.float()
+    # t means torch
+    tQ, tK, tV = [tensor.float() for tensor in input_tensors]
     tA = torch.matmul(tQ, tK)
     row_max = torch.max(tA, dim=-1, keepdim=True)[0]
     tA = tA - row_max
     tE = torch.exp(tA)
     tO1 = torch.matmul(tE, tV)
     tO2 = torch.sum(tE, dim=-1, keepdim=True)
-    tS = tO1 / tO2
+    tR = tO1 / tO2
 
     input_strides = [tensor.stride() for tensor in input_tensors]
     p = mi.generate_cuda_program(
@@ -61,23 +59,11 @@ if __name__ == "__main__":
         enable_online_softmax=True,
     )
     print(p["code"])
-    # warm up runs
-    for _ in range(16):
-        outputs = graph(inputs=input_tensors)
-    torch.cuda.synchronize()
 
-    starter, ender = torch.cuda.Event(enable_timing=True), torch.cuda.Event(
-        enable_timing=True
-    )
-    repetitions = 1000
-    timings = np.zeros((repetitions, 1))
-    starter.record()
-    for rep in range(repetitions):
-        outputs = graph(inputs=input_tensors)
-    ender.record()
-    torch.cuda.synchronize()
-    curr_time = starter.elapsed_time(ender)
-
-    mean_syn = curr_time / 1000
-    print(mean_syn)
-    print(outputs[0] / tS)
+    outputs = graph(inputs=input_tensors, enable_online_softmax=True)
+    print(outputs[0])
+    print(outputs[1])
+    print(tO1)
+    print(tO2)
+    R = outputs[0] / outputs[1]
+    print(R / tR)
