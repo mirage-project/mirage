f067ab5c1d72ebd89e2f13e81c634640b6eb5f65
Jinchen Jiang <145248565+undefined-c0der@users.noreply.github.com>
2025-05-25 17:13:29 -0400

    Templatize the `norm_linear` and the `silu_mul_linear` kernel (#274)
    * templatize the `norm_linear` kernel
    
    * templatize the `silu_mul_linear` kernel
    
    * modify interfaces and add tests
    
    * add some minor optimization
    
    * templatize the `linear` kernel
    
    * update template parameters
    
    * adjust parameter naming

--- include/mirage/persistent_kernel/tasks/linear.cuh
+++ include/mirage/persistent_kernel/tasks/linear.cuh
@@ -28,76 +28,115 @@ namespace kernel {
 
 using bfloat16 = type::bfloat16_t;
 
-template <typename T>
+template <typename T, int BATCH_SIZE, int OUTPUT_SIZE, int REDUCTION_SIZE>
 __device__ __forceinline__ void linear_kernel(void const *input_ptr,
                                               void const *weight_ptr,
                                               void *output_ptr) {
 
-  constexpr int chunk_size = 16 / sizeof(T);
-  constexpr int BATCH_SIZE = 1;
-  constexpr int HIDDEN_SIZE = 64;
-  constexpr int NUM_CHUNKS_A = 8;
-  constexpr int NUM_CHUNKS_B = 512;
-  constexpr int OUTPUT_SIZE = 64;
+  constexpr int CHUNK_SIZE = 16 / sizeof(T);
+  constexpr int TILE_SIZE = 64;
+  constexpr int FORLOOP_RANGE = REDUCTION_SIZE / TILE_SIZE;
 
-  constexpr int num_chunks = HIDDEN_SIZE / chunk_size;
+  constexpr int NUM_CHUNKS_A = BATCH_SIZE * TILE_SIZE / CHUNK_SIZE;
+  constexpr int NUM_CHUNKS_B = TILE_SIZE * OUTPUT_SIZE / CHUNK_SIZE;
 
-  constexpr int num_n = 4;
+  constexpr int CHUNKS_PER_ROW_A = TILE_SIZE / CHUNK_SIZE;
+  constexpr int CHUNKS_PER_ROW_B = OUTPUT_SIZE / CHUNK_SIZE;
+
+  constexpr int CHUNKS_PER_ROW_A_MASK = CHUNKS_PER_ROW_A - 1;
+  constexpr int CHUNKS_PER_ROW_B_MASK = CHUNKS_PER_ROW_B - 1;
+
+  constexpr int log2_CHUNK_SIZE = 3;
+  constexpr int log2_CHUNKS_PER_ROW_A = 3;
+  constexpr int log2_CHUNKS_PER_ROW_B = CHUNKS_PER_ROW_B == 2   ? 1
+                                        : CHUNKS_PER_ROW_B == 4 ? 2
+                                                                : 3;
+
+  // using SM80_16x8x16_F16F16F16F16_TNX2 = 16X16X16, threadLayout = 1,4,1
+  // ->16X64X16
   constexpr int num_m = 1;
-  constexpr int num_k = 4;
+  constexpr int num_n = OUTPUT_SIZE / 16;
+  constexpr int num_k = TILE_SIZE / 16;
+  constexpr int num_iters_n = (num_n + 3) >> 2;
   int warp_idx = warp_id();
   int idx_in_warp = threadIdx.x & 0x1F;
 
-  assert(num_m > 0 && num_n > 0 && num_k > 0);
-  __restrict__ T const *d_input = static_cast<T const *>(input_ptr);
+  T const *__restrict__ d_input = static_cast<T const *>(input_ptr);
+  T const *__restrict__ d_weight = static_cast<T const *>(weight_ptr);
+  T *__restrict__ d_output = static_cast<T *>(output_ptr);
 
-  __restrict__ T const *d_weight = static_cast<T const *>(weight_ptr);
-  T __restrict__ *d_output = static_cast<T *>(output_ptr);
+  using InputDmem = dmem_row_const<T, BATCH_SIZE, TILE_SIZE, REDUCTION_SIZE>;
+  using WeightDmem = dmem_row_const<T, TILE_SIZE, OUTPUT_SIZE, OUTPUT_SIZE>;
+  using OutputDmem = dmem_row<T, BATCH_SIZE, OUTPUT_SIZE, OUTPUT_SIZE>;
 
-  dmem_row_const<T, 1, 64, 3584> input_dmem(d_input);
-  dmem_row_const<T, 64, 64, 64> weight_dmem(d_weight);
-  dmem_row<T, 1, 64, 64> output_dmem(d_output);
+  InputDmem input_dmem(d_input);
+  WeightDmem weight_dmem(d_weight);
+  OutputDmem output_dmem(d_output);
 
   extern __shared__ char smem[];
 
+  // zero buffer
+  *((__uint128_t *)smem) = 0ul;
+  T *zero_buf = (T *)(smem + 0); // 128 bytes
+
   // copy input
-  T *shared_input = (T *)(smem + 128);
-  T *shared_input_buffer = (T *)(smem + 8448);
+  T *shared_input = (T *)(smem + 128); // sizeof(T) * BATCH_SIZE * TILE_SIZE
+  T *shared_input_buffer =
+      (T *)(smem + 128 +
+            sizeof(T) * BATCH_SIZE *
+                TILE_SIZE); // sizeof(T) * BATCH_SIZE * TILE_SIZE
+
   // copy weight
-  T *shared_weight = (T *)(smem + 8704);
-  T *shared_weight_buffer = (T *)(smem + 256);
+  T *shared_weight =
+      (T *)(smem + 128 + sizeof(T) * BATCH_SIZE * TILE_SIZE +
+            sizeof(T) * BATCH_SIZE *
+                TILE_SIZE); // sizeof(T) * TILE_SIZE * OUTPUT_SIZE
+  T *shared_weight_buffer =
+      (T *)(smem + 128 + sizeof(T) * BATCH_SIZE * TILE_SIZE +
+            sizeof(T) * BATCH_SIZE * TILE_SIZE +
+            sizeof(T) * TILE_SIZE *
+                OUTPUT_SIZE); // sizeof(T) * TILE_SIZE * OUTPUT_SIZE
 
   // out
-  T *shared_output = (T *)(smem + 128);
-  T *zero_buf = (T *)(smem);
+  T *shared_output =
+      (T *)(smem + 128 + sizeof(T) * BATCH_SIZE * TILE_SIZE +
+            sizeof(T) * BATCH_SIZE * TILE_SIZE +
+            sizeof(T) * TILE_SIZE * OUTPUT_SIZE +
+            sizeof(T) * TILE_SIZE * OUTPUT_SIZE); // sizeof(T) * BATCH_SIZE *
+                                                  // OUTPUT_SIZE
 
   // define the swizzle mode
+
+  using ZeroBufferSmem = smem_row<T, 0, 0, 0, 1, 8, 8>;
+  using InputSmem = smem_row<T, 0, 0, 0, BATCH_SIZE, TILE_SIZE, TILE_SIZE>;
+  using WeightSmem = smem_row<T, 3, 3, 3, TILE_SIZE, OUTPUT_SIZE, OUTPUT_SIZE>;
+  using OutputSmem = smem_row<T, 0, 0, 0, BATCH_SIZE, OUTPUT_SIZE, OUTPUT_SIZE>;
+
   // zero buffer
-  smem_row<T, 1, 1, 1, 1, 8, 8> zero_buffer(zero_buf);
+  ZeroBufferSmem zero_buffer(zero_buf);
 
-  smem_row<T, 1, 1, 1, 1, 64, 64> input_smem(shared_input);
-  smem_row<T, 1, 1, 1, 1, 64, 64> input_smem_buffer(shared_input_buffer);
+  InputSmem input_smem(shared_input);
+  InputSmem input_smem_buffer(shared_input_buffer);
 
-  smem_row<T, 3, 3, 3, 64, 64, 64> input_weight_smem(shared_weight);
-  smem_row<T, 3, 3, 3, 64, 64, 64> input_weight_smem_buffer(
-      shared_weight_buffer);
+  WeightSmem input_weight_smem(shared_weight);
+  WeightSmem input_weight_smem_buffer(shared_weight_buffer);
 
-  smem_row<T, 1, 1, 1, 1, 64, 64> output_smem(shared_output);
+  OutputSmem output_smem(shared_output);
 
 // load input
 #pragma unroll
   for (int i = threadIdx.x; i < NUM_CHUNKS_A; i += NUM_THREADS) {
     // offset
-    int row = i >> 3;
-    int col = (i & 0x7) << 3;
+    int row = i >> log2_CHUNKS_PER_ROW_A;
+    int col = (i & CHUNKS_PER_ROW_A_MASK) << log2_CHUNK_SIZE;
     load_smem(input_smem_buffer(row, col), input_dmem(row, col));
   }
 
 // load weight
 #pragma unroll
   for (int i = threadIdx.x; i < NUM_CHUNKS_B; i += NUM_THREADS) {
-    int row = i >> 3;
-    int col = (i & 0x7) << 3;
+    int row = i >> log2_CHUNKS_PER_ROW_B;
+    int col = (i & CHUNKS_PER_ROW_B_MASK) << log2_CHUNK_SIZE;
     load_smem(input_weight_smem_buffer(row, col), weight_dmem(row, col));
   }
   cp_async_fence();
@@ -110,26 +149,25 @@ __device__ __forceinline__ void linear_kernel(void const *input_ptr,
     zero_buffer.at(0, i) = bfloat16(0.0f);
   }
 
-  for (int for_idx = 0; for_idx < 56; for_idx++) {
+  for (int for_idx = 0; for_idx < FORLOOP_RANGE; for_idx++) {
     // copy
-    if (for_idx + 1 != 56) {
-      dmem_row_const<T, 1, 64, 3584> input_dmem_buffer(d_input +
-                                                       64 * (for_idx + 1));
-      dmem_row_const<T, 64, 64, 64> weight_dmem_buffer(d_weight +
-                                                       4096 * (for_idx + 1));
+    if (for_idx + 1 != FORLOOP_RANGE) {
+      InputDmem input_dmem_buffer(d_input + TILE_SIZE * (for_idx + 1));
+      WeightDmem weight_dmem_buffer(d_weight +
+                                    TILE_SIZE * OUTPUT_SIZE * (for_idx + 1));
 
 #pragma unroll
       for (int i = threadIdx.x; i < NUM_CHUNKS_A; i += NUM_THREADS) {
         // offset
-        int row = i >> 3;
-        int col = (i & 0x7) << 3;
+        int row = i >> log2_CHUNKS_PER_ROW_A;
+        int col = (i & CHUNKS_PER_ROW_A_MASK) << log2_CHUNK_SIZE;
         load_smem(input_smem(row, col), input_dmem_buffer(row, col));
       }
 // load weight
 #pragma unroll
       for (int i = threadIdx.x; i < NUM_CHUNKS_B; i += NUM_THREADS) {
-        int row = i >> 3;
-        int col = (i & 0x7) << 3;
+        int row = i >> log2_CHUNKS_PER_ROW_B;
+        int col = (i & CHUNKS_PER_ROW_B_MASK) << log2_CHUNK_SIZE;
         load_smem(input_weight_smem(row, col), weight_dmem_buffer(row, col));
       }
       cp_async_fence();
@@ -150,44 +188,47 @@ __device__ __forceinline__ void linear_kernel(void const *input_ptr,
     }
 
     __syncthreads();
-    uint32_t a_frag[4], b_frag[4];
 
-    for (uint32_t n = 0; n < num_n / 4; n++) {
-      for (uint32_t m = 0; m < num_m; m++) {
-
-        int m_row = idx_in_warp & 0xF;
+    if (warp_idx < num_n) {
+      uint32_t a_frag[4], b_frag[4];
+      for (uint32_t n = 0; n < num_iters_n; n++) {
         int n_col = (warp_idx << 4) + ((idx_in_warp >> 4) << 3);
+        for (uint32_t m = 0; m < num_m; m++) {
+          int m_row = (idx_in_warp & 0xF);
 #pragma unroll
-        for (uint32_t k = 0; k < num_k; k++) {
-          int m_col = (k << 4) + ((idx_in_warp >> 4) << 3);
-          // load B matrix,
-          int n_row = (idx_in_warp & 0xF) + (k << 4);
-          // load from a all zero mem
-          bool is_valid = (m_row < BATCH_SIZE);
-          T *src_ptr = is_valid ? input_smem(m_row, m_col) : zero_buffer(0, 0);
-          ldsm(src_ptr, &a_frag[0]);
-          ldsm_t(input_weight_smem(n_row, n_col), &b_frag[0]);
-          mma_m16n16k16_bf16bf16bf32(
-              s_frag[m][n], a_frag, b_frag, s_frag[m][n]);
+          for (uint32_t k = 0; k < num_k; k++) {
+            int m_col = (k << 4) + ((idx_in_warp >> 4) << 3);
+            int n_row = (k << 4) + (idx_in_warp & 0xF);
+            bool is_valid = (m_row < BATCH_SIZE);
+            T *src_ptr =
+                is_valid ? input_smem(m_row, m_col) : zero_buffer(0, 0);
+            ldsm(src_ptr, a_frag);
+            ldsm_t(input_weight_smem(n_row, n_col), b_frag);
+            mma_m16n16k16_bf16bf16bf32(
+                s_frag[m][n], a_frag, b_frag, s_frag[m][n]);
+          }
         }
       }
     }
 
     __syncthreads();
   }
-  // reg write back to smem
 
-  for (uint32_t n = 0; n < num_n / 4; n++) {
-    for (uint32_t m = 0; m < num_m; m++) {
+  // reg write back to smem
+  if (warp_idx < num_n) {
+    for (uint32_t n = 0; n < num_iters_n; n++) {
+      for (uint32_t m = 0; m < num_m; m++) {
 #pragma unroll
-      for (uint32_t i = 0; i < 4; i++) {
-        int row = (idx_in_warp >> 2) + ((i & 0x1) << 3);
-        if (row < BATCH_SIZE) {
-          // continue;
-          int col =
-              ((idx_in_warp & 0x3) << 1) + (warp_idx << 4) + ((i >> 1) << 3);
-          output_smem.at(row, col) = bfloat16(s_frag[m][n][i * 2]);
-          output_smem.at(row, col + 1) = bfloat16(s_frag[m][n][i * 2 + 1]);
+        for (uint32_t i = 0; i < 4; i++) {
+          int row = (idx_in_warp >> 2) + ((i & 0x1) << 3);
+          if (row < BATCH_SIZE) {
+            // continue;
+            int col =
+                (warp_idx << 4) + ((idx_in_warp & 0x3) << 1) + ((i >> 1) << 3);
+            output_smem.at(row, col) = bfloat16(s_frag[m][n][(i << 1)]);
+            output_smem.at(row, col + 1) =
+                bfloat16(s_frag[m][n][(i << 1) | 0x1]);
+          }
         }
       }
     }
--- tests/runtime_python/runtime_kernel_wrapper.cu
+++ tests/runtime_python/runtime_kernel_wrapper.cu
@@ -50,6 +50,19 @@ using bfloat16 = type::bfloat16_t;
     printf("Unsupported seq_len: %zu\n", SEQ_LEN);                             \
   }
 
+#define DISPATCH_OUTPUT_SIZE(OUTPUT_SIZE, FUNC, T, ...)                        \
+  if ((OUTPUT_SIZE) == 16) {                                                   \
+    FUNC<T, 1, 16, 4096>(__VA_ARGS__);                                         \
+  } else if ((OUTPUT_SIZE) == 32) {                                            \
+    FUNC<T, 1, 32, 4096>(__VA_ARGS__);                                         \
+  } else if ((OUTPUT_SIZE) == 64) {                                            \
+    FUNC<T, 1, 64, 4096>(__VA_ARGS__);                                         \
+  } else {                                                                     \
+    printf("Unsupported output size: %d\n", OUTPUT_SIZE);                      \
+  }
+
+// Single Batch Decoding
+
 template <typename T, size_t SEQ_LEN>
 __global__ void single_batch_decoding_kernel_wrapper(void const *qkv_ptr,
                                                      void *k_cache_ptr,
@@ -102,31 +115,31 @@ void single_batch_decoding(torch::Tensor qkv,
   }
 }
 
-template <typename T>
+// RMSNorm Linear
+
+template <typename T, int BATCH_SIZE, int OUTPUT_SIZE, int REDUCTION_SIZE>
 __global__ void norm_linear_kernel_wrapper(void const *input_ptr,
                                            void const *weight_ptr,
                                            void *output_ptr) {
-  norm_linear_kernel<T>(input_ptr, weight_ptr, output_ptr);
+  norm_linear_kernel<T, BATCH_SIZE, OUTPUT_SIZE, REDUCTION_SIZE>(
+      input_ptr, weight_ptr, output_ptr);
 }
 
-template <typename T>
-__global__ void silu_mul_linear_kernel_wrapper(void const *input_ptr,
-                                               void const *mul_ptr,
-                                               void const *weight_ptr,
-                                               void *output_ptr) {
-  silu_mul_linear_kernel<T>(input_ptr, mul_ptr, weight_ptr, output_ptr);
-}
+template <typename T, int BATCH_SIZE, int OUTPUT_SIZE, int REDUCTION_SIZE>
+void launch_norm_linear(void const *input_ptr,
+                        void const *weight_ptr,
+                        void *output_ptr) {
+  dim3 grid_dim(1, 1, 1);
+  dim3 block_dim(128, 1, 1);
+  size_t smem_size = 36666;
 
-template <typename T>
-__global__ void linear_kernel_wrapper(void const *input_ptr,
-                                      void const *weight_ptr,
-                                      void *output_ptr) {
-  linear_kernel<T>(input_ptr, weight_ptr, output_ptr);
-}
+  cudaFuncSetAttribute(
+      norm_linear_kernel_wrapper<T, BATCH_SIZE, OUTPUT_SIZE, REDUCTION_SIZE>,
+      cudaFuncAttributeMaxDynamicSharedMemorySize,
+      smem_size);
 
-template <typename T>
-__global__ void argmax_kernel_wrapper(void const *input_ptr, void *output_ptr) {
-  argmax_kernel<T, 1, 32768>(input_ptr, output_ptr);
+  norm_linear_kernel_wrapper<T, BATCH_SIZE, OUTPUT_SIZE, REDUCTION_SIZE>
+      <<<grid_dim, block_dim, smem_size>>>(input_ptr, weight_ptr, output_ptr);
 }
 
 void norm_linear(torch::Tensor input,
@@ -137,18 +150,49 @@ void norm_linear(torch::Tensor input,
   void const *weight_ptr = weight.data_ptr();
   void *output_ptr = output.data_ptr();
 
-  dim3 grid_dim(1, 1, 1);
-  dim3 block_dim(128, 1, 1);
-  cudaFuncSetAttribute(norm_linear_kernel_wrapper<bfloat16>,
-                       cudaFuncAttributeMaxDynamicSharedMemorySize,
-                       36666);
-  norm_linear_kernel_wrapper<bfloat16>
-      <<<grid_dim, block_dim, 36666>>>(input_ptr, weight_ptr, output_ptr);
-  cudaDeviceSynchronize();
+  DISPATCH_OUTPUT_SIZE(output.size(1),
+                       launch_norm_linear,
+                       bfloat16,
+                       input_ptr,
+                       weight_ptr,
+                       output_ptr);
+
   cudaError_t err = cudaDeviceSynchronize();
   if (err != cudaSuccess) {
     printf("CUDA kernel launch error: %s\n", cudaGetErrorString(err));
   }
+}
+
+// SiLU MUL Linear
+
+template <typename T, int BATCH_SIZE, int OUTPUT_SIZE, int REDUCTION_SIZE>
+__global__ void silu_mul_linear_kernel_wrapper(void const *input_ptr,
+                                               void const *mul_ptr,
+                                               void const *weight_ptr,
+                                               void *output_ptr) {
+  silu_mul_linear_kernel<T, BATCH_SIZE, OUTPUT_SIZE, REDUCTION_SIZE>(
+      input_ptr, mul_ptr, weight_ptr, output_ptr);
+}
+
+template <typename T, int BATCH_SIZE, int OUTPUT_SIZE, int REDUCTION_SIZE>
+void launch_silu_mul_linear(void const *input_ptr,
+                            void const *mul_ptr,
+                            void const *weight_ptr,
+                            void *output_ptr) {
+  dim3 grid_dim(1, 1, 1);
+  dim3 block_dim(128, 1, 1);
+  size_t smem_size = 36666;
+
+  cudaFuncSetAttribute(silu_mul_linear_kernel_wrapper<T,
+                                                      BATCH_SIZE,
+                                                      OUTPUT_SIZE,
+                                                      REDUCTION_SIZE>,
+                       cudaFuncAttributeMaxDynamicSharedMemorySize,
+                       smem_size);
+
+  silu_mul_linear_kernel_wrapper<T, BATCH_SIZE, OUTPUT_SIZE, REDUCTION_SIZE>
+      <<<grid_dim, block_dim, smem_size>>>(
+          input_ptr, mul_ptr, weight_ptr, output_ptr);
 }
 
 void silu_mul_linear(torch::Tensor input,
@@ -161,18 +205,45 @@ void silu_mul_linear(torch::Tensor input,
   void const *weight_ptr = weight.data_ptr();
   void *output_ptr = output.data_ptr();
 
-  dim3 grid_dim(1, 1, 1);
-  dim3 block_dim(128, 1, 1);
-  cudaFuncSetAttribute(silu_mul_linear_kernel_wrapper<bfloat16>,
-                       cudaFuncAttributeMaxDynamicSharedMemorySize,
-                       36666);
-  silu_mul_linear_kernel_wrapper<bfloat16><<<grid_dim, block_dim, 36666>>>(
-      input_ptr, mul_ptr, weight_ptr, output_ptr);
-  cudaDeviceSynchronize();
+  DISPATCH_OUTPUT_SIZE(output.size(1),
+                       launch_silu_mul_linear,
+                       bfloat16,
+                       input_ptr,
+                       mul_ptr,
+                       weight_ptr,
+                       output_ptr);
+
   cudaError_t err = cudaDeviceSynchronize();
   if (err != cudaSuccess) {
     printf("CUDA kernel launch error: %s\n", cudaGetErrorString(err));
   }
+}
+
+// Linear
+
+template <typename T, int BATCH_SIZE, int OUTPUT_SIZE, int REDUCTION_SIZE>
+__global__ void linear_kernel_wrapper(void const *input_ptr,
+                                      void const *weight_ptr,
+                                      void *output_ptr) {
+  linear_kernel<T, BATCH_SIZE, OUTPUT_SIZE, REDUCTION_SIZE>(
+      input_ptr, weight_ptr, output_ptr);
+}
+
+template <typename T, int BATCH_SIZE, int OUTPUT_SIZE, int REDUCTION_SIZE>
+void launch_linear(void const *input_ptr,
+                   void const *weight_ptr,
+                   void *output_ptr) {
+  dim3 grid_dim(1, 1, 1);
+  dim3 block_dim(128, 1, 1);
+  size_t smem_size = 36666;
+
+  cudaFuncSetAttribute(
+      linear_kernel_wrapper<T, BATCH_SIZE, OUTPUT_SIZE, REDUCTION_SIZE>,
+      cudaFuncAttributeMaxDynamicSharedMemorySize,
+      smem_size);
+
+  linear_kernel_wrapper<T, BATCH_SIZE, OUTPUT_SIZE, REDUCTION_SIZE>
+      <<<grid_dim, block_dim, smem_size>>>(input_ptr, weight_ptr, output_ptr);
 }
 
 void linear(torch::Tensor input, torch::Tensor weight, torch::Tensor output) {
@@ -181,18 +252,38 @@ void linear(torch::Tensor input, torch::Tensor weight, torch::Tensor output) {
   void const *weight_ptr = weight.data_ptr();
   void *output_ptr = output.data_ptr();
 
-  dim3 grid_dim(1, 1, 1);
-  dim3 block_dim(128, 1, 1);
-  cudaFuncSetAttribute(linear_kernel_wrapper<bfloat16>,
-                       cudaFuncAttributeMaxDynamicSharedMemorySize,
-                       36666);
-  linear_kernel_wrapper<bfloat16>
-      <<<grid_dim, block_dim, 36666>>>(input_ptr, weight_ptr, output_ptr);
-  cudaDeviceSynchronize();
+  DISPATCH_OUTPUT_SIZE(output.size(1),
+                       launch_linear,
+                       bfloat16,
+                       input_ptr,
+                       weight_ptr,
+                       output_ptr);
+
   cudaError_t err = cudaDeviceSynchronize();
   if (err != cudaSuccess) {
     printf("CUDA kernel launch error: %s\n", cudaGetErrorString(err));
   }
+}
+
+// Argmax
+
+template <typename T>
+__global__ void argmax_kernel_wrapper(void const *input_ptr, void *output_ptr) {
+  argmax_kernel<T, 1, 32768>(input_ptr, output_ptr);
+}
+
+template <typename T>
+void launch_argmax(void const *input_ptr, void *output_ptr) {
+  dim3 grid_dim(1, 1, 1);
+  dim3 block_dim(128, 1, 1);
+  size_t smem_size = 36666;
+
+  cudaFuncSetAttribute(argmax_kernel_wrapper<T>,
+                       cudaFuncAttributeMaxDynamicSharedMemorySize,
+                       smem_size);
+
+  argmax_kernel_wrapper<T>
+      <<<grid_dim, block_dim, smem_size>>>(input_ptr, output_ptr);
 }
 
 void argmax(torch::Tensor input, torch::Tensor output) {
@@ -200,14 +291,8 @@ void argmax(torch::Tensor input, torch::Tensor output) {
   void const *input_ptr = input.data_ptr();
   void *output_ptr = output.data_ptr();
 
-  dim3 grid_dim(1, 1, 1);
-  dim3 block_dim(128, 1, 1);
-  cudaFuncSetAttribute(argmax_kernel_wrapper<bfloat16>,
-                       cudaFuncAttributeMaxDynamicSharedMemorySize,
-                       36666);
-  argmax_kernel_wrapper<bfloat16>
-      <<<grid_dim, block_dim, 36666>>>(input_ptr, output_ptr);
-  cudaDeviceSynchronize();
+  launch_argmax<bfloat16>(input_ptr, output_ptr);
+
   cudaError_t err = cudaDeviceSynchronize();
   if (err != cudaSuccess) {
     printf("CUDA kernel launch error: %s\n", cudaGetErrorString(err));
@@ -215,6 +300,7 @@ void argmax(torch::Tensor input, torch::Tensor output) {
 }
 
 // pybind11 bindings
+
 PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
   m.def("linear", &linear, "Linear kernel");
   m.def("argmax", &argmax, "argmax kernel");
