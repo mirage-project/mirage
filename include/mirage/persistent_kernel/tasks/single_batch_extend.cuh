b9a9f9de7de794db78a0b4a3f18079758d5a6abf
Jianan Ji <jji2@andrew.cmu.edu>
2025-07-03 19:57:23 +0000

    progress

--- include/mirage/persistent_kernel/tasks/single_batch_extend.cuh
+++ include/mirage/persistent_kernel/tasks/single_batch_extend.cuh
@@ -58,8 +58,10 @@ __device__ __forceinline__ void
   int warp_idx = warp_id();
   int idx_in_warp = threadIdx.x % 32;
 
-  size_t num_iterations = (seq_len + KV_CHUNK_SIZE - 1) / KV_CHUNK_SIZE;
-  int curr_iter_len = std::min(seq_len, KV_CHUNK_SIZE);
+  size_t total_seq_len = seq_len + EXTEND_NUM;
+
+  size_t num_iterations = (total_seq_len + KV_CHUNK_SIZE - 1) / KV_CHUNK_SIZE;
+  int curr_iter_len = std::min(total_seq_len, KV_CHUNK_SIZE);
   int cp_finished_seq_len = curr_iter_len;
   int last_seq_len = curr_iter_len;
 
@@ -93,7 +95,7 @@ __device__ __forceinline__ void
   constexpr size_t O_OFFSET = MAX_OFFSET + HEAD_DIM * sizeof(float);
 
   constexpr size_t Q_NORM_SUM_OFFSET = O_OFFSET + HEAD_DIM * 32 * sizeof(float); // TODO: check
-  constexpr size_t K_NORM_SUM_OFFSET = Q_NORM_SUM_OFFSET + HEAD_DIM * sizeof(float);
+  constexpr size_t K_NORM_SUM_OFFSET = Q_NORM_SUM_OFFSET + NUM_WARPS * sizeof(float);
   
   constexpr size_t SHARED_OUTPUT_OFFSET = 128;
   constexpr size_t ZERO_BUFFER_OFFSET = 0;
@@ -140,6 +142,176 @@ __device__ __forceinline__ void
   for (int i = 0; i < 8; i++) {
     zero_buffer.at(i) = (bfloat16)0.0f;
   }
+
+  // load first Q, K, V
+#pragma unroll
+  for (int i = threadIdx.x; i < NUM_Q_HEADS * EXTEND_NUM * (HEAD_DIM / 8);
+       i += NUM_THREADS) {
+    // offset
+    int row = i / 16;
+    int col = (i % 16) * 8;
+    load_smem(q_smem(row, col), q_dmem(row, col));
+  }
+
+  // metadata for flashattention
+  // TODO: check if this is enough
+  float o[8][8];
+#pragma unroll
+  for (int n = 0; n < 8; n++) {
+    clear_8_floats(o[n]);
+  }
+  float d_sum = 1.f;
+  float m = -inf;
+
+#pragma unroll
+  for (int i = threadIdx.x; i < (curr_iter_len * 16); i += NUM_THREADS) {
+    // offset
+    int row = i / 16;
+    int col = (i % 16) * 8;
+    if (row >= seq_len - 1) { // last and extended tokens
+      // from qkv
+      load_smem(k_cache_smem_buffer(row, col), k_dmem(row - (seq_len - 1), col));
+    } else {
+      // from cache
+      load_smem(k_cache_smem_buffer(row, col), k_cache_dmem(row, col));
+    }
+  }
+
+  #pragma unroll
+  for (int i = threadIdx.x; i < (curr_iter_len * 16); i += NUM_THREADS) {
+    // offset
+    int row = i / 16;
+    int col = (i % 16) * 8;
+    if (row >= seq_len - 1) { // last and extended tokens
+      load_smem(v_cache_smem_buffer(row, col), v_dmem(row - (seq_len - 1), col));
+    } else {
+      load_smem(v_cache_smem_buffer(row, col), v_cache_dmem(row, col));
+    }
+  }
+  cp_async_fence();
+
+  // KV iteration
+  //  N = 64 per iter
+  for (uint32_t kv_idx = 0; kv_idx < num_iterations; kv_idx += 1) {
+    // load next k, v
+    int next_iter_len =
+        kv_idx + 1 < num_iterations
+            ? static_cast<int>(std::min(total_seq_len, (kv_idx + 2) * KV_CHUNK_SIZE) -
+                               (kv_idx + 1) * KV_CHUNK_SIZE)
+            : -1;
+
+      // async load next k, v
+      if (kv_idx + 1 != num_iterations) {
+#pragma unroll
+      for (int i = threadIdx.x; i < (next_iter_len * 16); i += NUM_THREADS) {
+        // offset
+        int row = i / 16;
+        int col = (i % 16) * 8;
+        if (row + cp_finished_seq_len >= seq_len - 1) {
+          load_smem(k_cache_smem(row, col), k_dmem(row + cp_finished_seq_len - (seq_len - 1), col));
+        } else {
+          load_smem(k_cache_smem(row, col),
+                    k_cache_dmem(cp_finished_seq_len + row, col));
+        }
+      }
+#pragma unroll
+      for (int i = threadIdx.x; i < (next_iter_len * 16); i += NUM_THREADS) {
+        // offset
+        int row = i / 16;
+        int col = (i % 16) * 8;
+        if (row + cp_finished_seq_len >= seq_len - 1) { 
+          load_smem(v_cache_smem(row, col), v_dmem(row + cp_finished_seq_len - (seq_len - 1), col));
+        } else {
+          load_smem(v_cache_smem(row, col),
+                    v_cache_dmem(cp_finished_seq_len + row, col));
+        }
+      }
+      cp_async_fence();
+      cp_async_wait<1>();
+    } else {
+      cp_async_wait<0>();
+    }
+
+    if ((kv_idx & 1) == 0) {
+      k_cache_smem.set_ptr(shared_k_buffer);
+      k_cache_smem_buffer.set_ptr(shared_k);
+      v_cache_smem.set_ptr(shared_v_buffer);
+      v_cache_smem_buffer.set_ptr(shared_v);
+    } else {
+      k_cache_smem.set_ptr(shared_k);
+      k_cache_smem_buffer.set_ptr(shared_k_buffer);
+      v_cache_smem.set_ptr(shared_v);
+      v_cache_smem_buffer.set_ptr(shared_v_buffer);
+    }
+    __syncthreads();
+
+    // q_norm
+    if (qk_norm && kv_idx == 0) {
+      window_rms_norm<T, QSmem, NUM_Q_HEADS, EXTEND_NUM + 1, HEAD_DIM>(
+          q_smem,
+          static_cast<T const *>(qnorm_weight_ptr),
+          qnorm_sum,
+          q_eps,
+          rotary_emd,
+          static_cast<T const *>(cos_ptr) + (seq_len - 1) * HEAD_DIM, //TODO: check the index of cos and sin
+          static_cast<T const *>(sin_ptr) + (seq_len - 1) * HEAD_DIM);
+    }
+
+    // knorm
+    if (qk_norm && kv_idx == num_iterations - 1) {
+      window_rms_norm<T, KSmem, NUM_KV_HEADS, EXTEND_NUM + 1, HEAD_DIM>(
+          k_cache_smem,
+          static_cast<T const *>(knorm_weight_ptr),
+          knorm_sum,
+          k_eps,
+          rotary_emd,
+          static_cast<T const *>(cos_ptr) + (seq_len - 1) * HEAD_DIM, //TODO: check the index of cos and sin
+          static_cast<T const *>(sin_ptr) + (seq_len - 1) * HEAD_DIM);
+    }
+    __syncthreads();
+
+    // MMA
+    constexpr int NUM_Q_TOKEN_DIM_ITER = (NUM_Q_HEADS * EXTEND_NUM + 16 - 1) / 16;
+    float s_frag[NUM_Q_TOKEN_DIM_ITER][8];
+    #pragma unroll
+    for(int i = 0; i < NUM_Q_TOKEN_DIM_ITER; i++) {
+      clear_8_floats(s_frag[i]);
+    }
+
+    uint32_t a_frag[4], b_frag[4], v_frag[4];
+
+    // QK^T
+    //  MNK = 4 * (extend_num + 1), 64, 128, tiledMMA 16, 64, 16, thread layout ()
+    //  int n_col = warp_idx * 16 + idx_in_warp / 16 * 8;
+    int n_row = (idx_in_warp / 16) * 8 + (idx_in_warp % 8) + warp_idx * 16;
+
+    #pragma unroll
+    for (uint32_t k = 0; k < 8; k++) {
+
+      int m_col = k * 16 + idx_in_warp / 16 * 8;
+      int n_col = ((idx_in_warp % 16) / 8) * 8 + k * 16;
+
+      #pragma unroll
+      for(int q_head_i = 0; q_head_i < NUM_Q_TOKEN_DIM_ITER; q_head_i++) {
+
+        int m_row = idx_in_warp % 16 + q_head_i * 16;
+
+        bool is_valid_A = (m_row < NUM_Q_HEADS * EXTEND_NUM);
+        T *src_ptr_A = is_valid_A ? q_smem(m_row, m_col) : zero_buffer(0, 0);
+        ldsm(src_ptr_A, &a_frag[0]);
+
+        bool is_valid_B = (n_row < curr_iter_len);
+        T *src_ptr_B =
+            is_valid_B ? k_cache_smem(n_row, n_col) : zero_buffer(0, 0);
+        ldsm(src_ptr_B, &b_frag[0]);
+
+        mma_m16n16k16_bf16bf16bf32(s_frag, a_frag, b_frag, s_frag);
+      }
+    }
+    __syncthreads();
+    // To be continued...
+  }
+
 
 
   // To be continued...
