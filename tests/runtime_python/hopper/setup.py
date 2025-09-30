7dd11c613dfe91d19fcdd7739112d4dfa559938e
yuzhou2 <yuzhou2@andrew.cmu.edu>
2025-09-30 20:21:27 +0000

    Adjust linear task partitioning

--- tests/runtime_python/hopper/setup.py
+++ tests/runtime_python/hopper/setup.py
@@ -34,11 +34,12 @@ setup(
             libraries=["cuda"],
             library_dirs=cuda_library_dirs,
             extra_compile_args={
-                'cxx': ['-DMIRAGE_GRACE_HOPPER'],
+                'cxx': ['-DMIRAGE_GRACE_HOPPER', '-DMIRAGE_BACKEND_USE_CUDA'],
                 'nvcc': [
                     '-O3',
                     '-gencode=arch=compute_90a,code=sm_90a',
                     '-DMIRAGE_GRACE_HOPPER',
+                    '-DMIRAGE_BACKEND_USE_CUDA',
                     # '-DMIRAGE_PROFILE_HOPPER',
                 ]
             }
