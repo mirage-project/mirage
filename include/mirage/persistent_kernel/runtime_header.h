9e67e02db01900d3a091399d79da855af0b7a797
Zhihao Jia <zhihao@cmu.edu>
2025-10-28 13:48:40 +0000

    init

--- include/mirage/persistent_kernel/runtime_header.h
+++ include/mirage/persistent_kernel/runtime_header.h
@@ -206,6 +206,7 @@ struct alignas(16) TaskDesc {
 struct RuntimeConfig {
   int num_workers, num_local_schedulers, num_remote_schedulers, num_graphs;
   int num_gpus, my_gpu_id;
+  int num_events;
   unsigned long long int per_worker_queue_len, per_sched_queue_len;
   unsigned long long int *worker_queue_last_ready_task_id;
   unsigned long long int *sched_queue_last_ready_event_id;
@@ -239,6 +240,7 @@ struct RuntimeConfig {
 #endif
   void *profiler_buffer;
   bool split_worker_scheduler;
+  cudaStream_t worker_stream, scheduler_stream;
 };
 
 } // namespace runtime
