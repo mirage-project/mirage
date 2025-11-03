31563f1672433f4a6f0005af531d1c3c960686b6
Zhihao Zhang <zhihaoz3@catalyst-fleet1.cs.cmu.edu>
2025-11-03 18:36:40 -0500

    moe linear updated

--- src/kernel/task_register.cc
+++ src/kernel/task_register.cc
@@ -1920,7 +1920,7 @@ int TaskRegister::register_moe_linear_sm100_task(
   mirage::transpiler::CodeKeeper code;
   code.inc_indent();
   // MoE constant:
-  constexpr int expert_stride = 12;
+  int expert_stride = (w13_linear) ? 12 : 9;
   // define MMA
   constexpr int MMA_M = 128;
   constexpr int MMA_N = 16;
