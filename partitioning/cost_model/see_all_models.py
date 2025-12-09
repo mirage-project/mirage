5014bd6f4b97542270e8700ad3264262cc483a71
AMKCode <aomankit2002@gmail.com>
2025-12-09 12:49:10 -0500

    removed see_all_models.py

--- partitioning/cost_model/see_all_models.py
+++
@@ -1,7 +0,0 @@
-from openai import OpenAI
-import os
-
-client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"), base_url="https://ai-gateway.andrew.cmu.edu/")
-models = client.models.list()
-for m in models:
-    print(m.id)
