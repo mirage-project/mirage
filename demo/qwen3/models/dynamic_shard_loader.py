de7631156b1b54a1d8be5c9d042e2fc92cdb69c4
Emily Ho <emilyho@catalyst-fleet1.cs.cmu.edu>
2026-02-24 00:17:44 -0500

    model loader tensor materialization logic

--- demo/qwen3/models/dynamic_shard_loader.py
+++ demo/qwen3/models/dynamic_shard_loader.py
@@ -7,16 +7,17 @@ from huggingface_hub import hf_hub_download
 import json
 import math
 from enum import Enum
+import torch
 
 class ShardType(Enum):
-    COL_PARALLEL = 0
-    ROW_PARALLEL = 1
+    ROW_PARALLEL = 0
+    COL_PARALLEL = 1
     EXPERT_PARALLEL = 2
     NONE = 100 # No sharding, replicate on all GPUs
 
 class DynamicShardLoader:
-    def __init__(self, model_obj, model_name, mapping, rank, world_size, download=False):
-        self.model_obj = model_obj
+    def __init__(self, model, model_name, mapping, rank, world_size, download=False):
+        self.model = model
         self.model_name = model_name
         self.mapping = mapping
         self.rank = rank
@@ -41,11 +42,13 @@ class DynamicShardLoader:
         for hf_filename in files_mapping:
             local_filepath = files_mapping[hf_filename]
 
-            # TODO (emily): handle loading onto other devices
-            with safe_open(local_filepath, framework="pt", device=f"cuda:{self.rank}") as f:
+            # TODO (emily): see if this should be loaded onto CPU or GPU
+            # Load onto CPU since materialization of tensor happens after getting the slice.
+            with safe_open(local_filepath, framework="pt", device="cpu") as f:
                 for name in f.keys():
+                    name_parts = name.split(".")
                     assert name.startswith("model.") or name == "lm_head.weight"
-                    key = name.split(".")[-2]
+                    key = name_parts[-2] # ex: q_proj
 
                     # TODO: either throw error (currently) or replicate on all GPUs if user didn't provide key
                     assert key in self.mapping, f"Param Key {key} not found in mapping"
@@ -57,23 +60,31 @@ class DynamicShardLoader:
                         if not self._check_expert_parallel(name, parallelism_info[ShardType.EXPERT_PARALLEL]):
                             continue
 
+                    # Get model module containing this weight. 
+                    module_path = ".".join(name_parts[:-1]) # ex: model.layers.0.mlp.experts.0.gate_proj
+                    module = self.model.get_submodule(module_path)
+
+                    meta_tensor = getattr(module, "weight")
+
                     # Allocate tensor on GPU & move data to it based on TP specifications.
                     weight_slice = f.get_slice(name)
                     if ShardType.COL_PARALLEL in parallelism_info:
                         tp_size = parallelism_info[ShardType.COL_PARALLEL]
-                        tensor = self.handle_tensor_parallelism(ShardType.COL_PARALLEL, tp_size, weight_slice, name)
+                        sharded_tensor, meta_shard = self.handle_tensor_parallelism(ShardType.COL_PARALLEL, tp_size, weight_slice, name, meta_tensor)
                     elif ShardType.ROW_PARALLEL in parallelism_info:
                         tp_size = parallelism_info[ShardType.ROW_PARALLEL]
-                        tensor = self.handle_tensor_parallelism(ShardType.ROW_PARALLEL, tp_size, weight_slice, name)
+                        sharded_tensor, meta_shard = self.handle_tensor_parallelism(ShardType.ROW_PARALLEL, tp_size, weight_slice, name, meta_tensor)
                     else: # No TP.
-                        tensor = weight_slice[:]
+                        sharded_tensor = weight_slice[:]
+                        meta_shard = meta_tensor
 
+                    # Attach tensor to model through materialization.
+                    self.materialize_and_attach_to_model(meta_shard, sharded_tensor, name, module)
 
-                    # TODO: Attach tensor to model
 
 
     def _get_parallelism_info(self, param_key):
-        """Given param key (ex: q_proj, gate), return parallelism info as a dictionary. 
+        """Given param key (ex: q_proj, gate), validate config and return parallelism info as a dictionary. 
         """
         mapping_info = self.mapping[param_key]
         parallelism_dict = {} # key: ShardType. val: num groups to parallelize by.
@@ -82,7 +93,35 @@ class DynamicShardLoader:
             if not isinstance(info, tuple):
                 info = (info,)
             
-            parallelism_dict[info[0]] = info[1] if len(info) > 1 else self.world_size
+            parallelism_dict[info[0]] = min(info[1], self.world_size) if len(info) > 1 else self.world_size
+
+        # Validate config. By default, this will priortize EP before TP. 
+        # This will only throw error if world size is not divisible by TP and EP. Otherwise, it'll update the config to the optimal config.
+        # Optimal config will ensure that ep * tp == world_size
+        if ShardType.EXPERT_PARALLEL in parallelism_dict:
+            ep_size = parallelism_dict[ShardType.EXPERT_PARALLEL]
+            assert ep_size <= self.world_size and self.world_size % ep_size == 0, f"world size not divisible by {param_key}'s EP configuration"
+
+            if parallelism_dict[ShardType.EXPERT_PARALLEL] > self.world_size:
+                parallelism_dict[ShardType.EXPERT_PARALLEL] = self.world_size
+                ep_size = self.world_size
+        else:
+            ep_size = 1
+
+        if ShardType.ROW_PARALLEL in parallelism_dict:
+            tp_size = parallelism_dict[ShardType.ROW_PARALLEL]
+            assert self.world_size % tp_size == 0, f"world size {self.world_size} not divisible by {param_key}'s TP configuration of {tp_size}"
+
+            if parallelism_dict[ShardType.ROW_PARALLEL] * ep_size != self.world_size:
+                parallelism_dict[ShardType.ROW_PARALLEL] = self.world_size // ep_size
+
+        if ShardType.COL_PARALLEL in parallelism_dict:
+            tp_size = parallelism_dict[ShardType.COL_PARALLEL]
+            assert self.world_size % tp_size == 0, f"world size {self.world_size} not divisible by {param_key}'s TP configuration of {tp_size}"
+
+            if parallelism_dict[ShardType.COL_PARALLEL] * ep_size != self.world_size:
+                parallelism_dict[ShardType.COL_PARALLEL] = self.world_size // ep_size
+
 
         return parallelism_dict
 
@@ -106,7 +145,7 @@ class DynamicShardLoader:
 
         weight_name_components = full_weight_name.split('.')
         weight_num = int(weight_name_components[5])
-        num_experts = self.model_obj.config.num_experts
+        num_experts = self.model.config.num_experts
         
         experts_per_rank = math.ceil(num_experts / self.world_size)
 
@@ -115,7 +154,17 @@ class DynamicShardLoader:
         return weight_num in range(expert_start, expert_end)
 
 
-    def handle_tensor_parallelism(self, tp_type, tp_size, weight_slice, weight_name):
+    def handle_tensor_parallelism(self, tp_type, tp_size, weight_slice, weight_name, meta_tensor):
+        """Perform sharding and create blueprint for the meta tensor.
+
+        Args:
+            - meta_tensor (PyTorch.Tensor): the meta tensor initialized in the model for this weight.
+        
+        Returns:
+            A tuple containing:
+                - PyTorch.Tensor of the sharded tensor
+                - A meta tensor with the correct shape (as blueprint for the actual tensor)
+        """
         dim = tp_type.value 
         
         # Valid shape with tensor parallel size.
@@ -130,10 +179,52 @@ class DynamicShardLoader:
         tp_rank = self.rank % tp_size
         start = tp_rank * shard_size
         end = (tp_rank + 1) * shard_size
+
+        # Get a meta tensor that is of the right shape.
+        meta_shard = meta_tensor.narrow(dim, start, shard_size)
+
         if tp_type == ShardType.COL_PARALLEL:
-            return weight_slice[start:end, :]
+            return weight_slice[:, start:end], meta_shard
         else:
-            return weight_slice[:, start:end]
+            return weight_slice[start:end, :], meta_shard
+
+
+    # TODO need to also handle cases where tp_type is None (where tp_size will then also be None).
+    def materialize_and_attach_to_model(self, meta_tensor, sharded_tensor, weight_name, module):
+        """Materialize the tensor in the model to point to the sharded tensor on device.
+        
+        Args:
+            - meta_tensor (PyTorch.Tensor): meta tensor to base the actual device tensor on (has the right shape / strides).
+            - sharded_tensor (PyTorch.Tensor): sharded tensor residing on the CPU (after calling get_slice()).
+            - weight_name (str): full weight name (ex: model.layers.0.mlp.experts.0.gate_proj.weights).
+        """
+        # Allocate memory for the actual tensor on the current device & copy tensor data.
+        device = torch.device(f"cuda:{self.rank}")
+        tensor = self.materialize_meta_tensor(meta_tensor, device)
+        with torch.no_grad():
+            tensor.copy_(sharded_tensor)
+
+        # Replace model's meta tensor with actual device tensor.
+        new_model_param = torch.nn.Parameter(tensor, requires_grad=meta_tensor.requires_grad)
+        setattr(module, "weight", new_model_param)
+        
+
+    def materialize_meta_tensor(self, meta_tensor: torch.Tensor, device: torch.device) -> torch.Tensor:
+        """ Adopted from vLLM's implementation.
+
+        Materialize a meta tensor into an actual tensor on the given device.
+        """
+        tensor = torch.empty_strided(
+            size=tuple(meta_tensor.size()),
+            stride=tuple(meta_tensor.stride()),
+            dtype=meta_tensor.dtype,
+            requires_grad=False,
+        )
+        tensor.__class__ = meta_tensor.__class__
+        tensor.__dict__ = meta_tensor.__dict__.copy()
+        return tensor
+
+
 
 
 
@@ -168,3 +259,5 @@ class DynamicShardLoader:
         
         self.files_mapping = files_mapping
         return files_mapping
+
+    
