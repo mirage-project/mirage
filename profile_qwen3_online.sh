68b8d87a6af73ecb894bb51599ab7378c97a241e
Shuaiwei Huang <shuaiweh@catalyst-fleet1.cs.cmu.edu>
2026-04-12 19:59:43 -0400

    update script and demo example for mpk online serving

--- profile_qwen3_online.sh
+++ profile_qwen3_online.sh
@@ -3,12 +3,16 @@
 # Nsys profiling script for Qwen3 demo
 # Usage: ./profile_qwen3.sh
 
-export CUDA_VISIBLE_DEVICES=0
+export CUDA_VISIBLE_DEVICES=4
+export TMPDIR=~/vllm/mirage/nvidia
 
 nsys profile \
     --trace=cuda,nvtx \
     --cuda-graph-trace=node \
     --output=mirage_online \
+    --capture-range=cudaProfilerApi \
+    --capture-range-end=stop \
     python demo/qwen3/demo_online.py \
         --max-num-batched-requests 6 \
         --max-seq-length 1024 \
+        --use-nsys
